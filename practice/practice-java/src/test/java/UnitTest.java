f17ac78fe59ef9f0c2b46d81552ff65a756f70f7
myungsubso <gkdldy5@naver.com>
2020-03-30 06:31:59 +0900

    Modify : .gitignore 수정

---
+++ practice/practice-java/src/test/java/UnitTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.*;
+
+public class UnitTest {
+
+  @Test
+  public void 파일변환_교육정리() throws Exception{
+    FileSystemResource input = new FileSystemResource("src/main/resources/edu.txt");
+    FileSystemResource output = new FileSystemResource("src/main/resources/eduOut.txt");
+    Path inPath = Paths.get(input.getURI());
+    final Path outPath = Paths.get(output.getPath());
+
+    try (BufferedReader reader = Files.newBufferedReader(inPath);
+         final OutputStream stream = Files.newOutputStream(outPath, StandardOpenOption.CREATE)) {
+
+      reader.lines()
+              .map(s -> s.replace("-", ""))
+              .map(s -> s.trim().replace(" ", ""))
+              .map(s -> s.replaceAll("/", ","))
+              .forEach(s -> {
+                try {
+                  stream.write(s.getBytes());
+                  stream.write(System.lineSeparator().charAt(0));
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              })
+      ;
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+  }
+}
