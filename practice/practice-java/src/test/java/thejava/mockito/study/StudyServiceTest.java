8015def54fd6528625b42d2683097e2e1a413bc5
myungsubso <gkdldy5@naver.com>
2020-04-17 07:04:51 +0900

    Add : Mockito BDD

--- practice/practice-java/src/test/java/thejava/mockito/study/StudyServiceTest.java
+++ practice/practice-java/src/test/java/thejava/mockito/study/StudyServiceTest.java
@@ -23,6 +23,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 
@@ -223,6 +225,34 @@ class StudyServiceTest {
 
 //    verify(memberService, times(1)).notify(any());
 //    memberService.notify();
+  }
+
+  @Test
+  @DisplayName("BDD 스타일 적용")
+  void mock_bdd() {
+
+    // given - member
+    StudyService studyService = new StudyService(memberService, studyRepository);
+    assertNotNull(studyService);
+
+    Member member = new Member();
+    member.setId(1L);
+    member.setEmail("ssosso@gmail.com");
+
+    Study study = new Study(10, "Test~!!");
+
+    given(memberService.findById(1L)).willReturn(Optional.of(member));
+    given(studyRepository.save(study)).willReturn(study);
+
+    // when
+    studyService.createNewStudy(1L, study);
+
+    // then
+    assertEquals(member, study.getMember());
+    then(memberService).should(times(1)).notify(study);
+//    then(memberService).shouldHaveNoMoreInteractions();
+//    then(memberService).shouldHaveNoInteractions();
+
   }
 
 }
