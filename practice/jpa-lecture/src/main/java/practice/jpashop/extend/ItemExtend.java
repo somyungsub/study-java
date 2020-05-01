d5a5ceaadb4361af26ee6d1bd0f91f8cf1a70fb8
myungsubso <gkdldy5@naver.com>
2020-05-02 08:49:38 +0900

    Add : JPA, @MappedSuperclass 사용법

--- practice/jpa-lecture/src/main/java/practice/jpashop/extend/ItemExtend.java
+++ practice/jpa-lecture/src/main/java/practice/jpashop/extend/ItemExtend.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Setter
@@ -12,7 +13,7 @@ import javax.persistence.*;
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE)   // 단일테이블 전략 : 하나의 테이블에 통합
 //@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)  // 구현 클래스별 테이블 전략 : 하위 클래스의 갯수 만큼 테이블 생성 : 상황에따라 abstract 삭제
 @DiscriminatorColumn// 컬럼명 : default -> DTYPE
-public abstract class ItemExtend {
+public class ItemExtend extends BaseEntity {
 
   @Id
   @GeneratedValue
