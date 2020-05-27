c31d1ee029d8f5a366b62fc66c058cd3af54cf36
myungsubso <gkdldy5@naver.com>
2020-05-28 07:30:34 +0900

    Add : 모던자바 인 액션, Pub/Sub, Processor 인터페이스 구현

--- book/모던자바인액션/src/main/java/chap17_FlowAPI/Main.java
+++ book/모던자바인액션/src/main/java/chap17_FlowAPI/Main.java
@@ -4,11 +4,20 @@ import java.util.concurrent.Flow.*;
 
 public class Main {
   public static void main(String[] args) {
-    getTemperatures("New York").subscribe(new TempSubscriber());
+//    getTemperatures("New York").subscribe(new TempSubscriber());
+    getCelsiusTemperatures("New York").subscribe(new TempSubscriber());
   }
 
   private static Publisher<TempInfo> getTemperatures(String town) {
     return subscriber -> subscriber.onSubscribe(new TempSubscription(subscriber, town));
 
+  }
+
+  private static Publisher<TempInfo> getCelsiusTemperatures(String town) {
+    return subscriber -> {
+      TempProcessor processor = new TempProcessor();
+      processor.subscribe(subscriber);
+      processor.onSubscribe(new TempSubscription(processor, town));
+    };
   }
 }
--- book/모던자바인액션/src/main/java/chap17_FlowAPI/TempSubscription.java
+++ book/모던자바인액션/src/main/java/chap17_FlowAPI/TempSubscription.java
@@ -1,8 +1,12 @@
 package chap17_FlowAPI;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow.*;
 
 public class TempSubscription implements Subscription {
+
+  private ExecutorService executor = Executors.newSingleThreadExecutor();
 
   private final Subscriber<? super TempInfo> subscriber;
   private final String town;
@@ -14,14 +18,17 @@ public class TempSubscription implements Subscription {
 
   @Override
   public void request(long n) {
-    for (int i = 0; i < n; i++) {
-      try {
-        subscriber.onNext(TempInfo.fetch(town));
-      } catch (Exception exception) {
-        subscriber.onError(exception);
-        break;
+
+    executor.submit(() -> {
+      for (int i = 0; i < n; i++) {
+        try {
+          subscriber.onNext(TempInfo.fetch(town));
+        } catch (Exception exception) {
+          subscriber.onError(exception);
+          break;
+        }
       }
-    }
+    });
   }
 
   @Override
