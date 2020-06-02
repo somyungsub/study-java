5629867d3e6dfca71038876d8821614fdb183378
myungsubso <gkdldy5@naver.com>
2020-06-03 08:44:54 +0900

    Add : 다이내믹 프로그래밍, 예제 - 최소비용경로 - 다이내믹 프로그래밍

--- book/다이내믹프로그래밍/src/main/java/chap04_DP적용전략/Code04.java
+++ book/다이내믹프로그래밍/src/main/java/chap04_DP적용전략/Code04.java
@@ -24,7 +24,9 @@ public class Code04 {
     return x < y ? x : y;
   }
 
-  int[][] mem = new int[3][4];
+  static final int M = 3;
+  static final int N = 4;
+  int[][] mem = new int[M][N];
 
   public int minPathCostMemo(int[][] cost, int m, int n) {
     if (mem[m][n] != 0) {
@@ -44,9 +46,34 @@ public class Code04 {
     }
 
     return mem[m][n];
-
-
   }
+
+  public int minPathCostDP(int[][] cost) {
+
+    // (0,0)
+    mem[0][0] = cost[0][0];
+
+    // 맨위 비용저장
+    for (int j = 1; j < N; j++) {
+      mem[0][j] = mem[0][j - 1] + cost[0][j];
+    }
+
+    // 맨왼쪽 비용저장
+    for (int i = 1; i < M; i++) {
+      mem[i][0] = mem[i - 1][0] + cost[i][0];
+    }
+
+    // 나머지 셀 비용 저장
+    for (int i = 1; i < M; i++) {
+      for (int j = 1; j < N; j++) {
+        mem[i][j] = getMin(mem[i - 1][j], mem[i][j - 1]) + cost[i][j];
+      }
+    }
+
+    return mem[M - 1][N - 1];
+  }
+
+
 
 
 }
--- book/다이내믹프로그래밍/src/test/java/chap04_DP적용전략/Code04Test.java
+++ book/다이내믹프로그래밍/src/test/java/chap04_DP적용전략/Code04Test.java
@@ -38,5 +38,15 @@ class Code04Test {
     System.out.println(code04.minPathCostMemo(cost, 2, 0));
 
   }
+  @Test
+  @DisplayName("예제 - 최소비용경로 [다이내미프로그래밍]")
+  public void 예제1_dp() {
+    int[][] cost = {
+        {1, 3, 5, 8},
+        {4, 2, 1, 7},
+        {4, 3, 2, 3}
+    };
+    System.out.println(code04.minPathCostDP(cost));
+  }
 
 }
