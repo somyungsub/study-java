1fbde01c5b18a32e034f09dae16859781b654802
myungsubso <gkdldy5@naver.com>
2020-06-04 08:56:29 +0900

    Add : RxJava 리액티브프로그래밍, compositeDisposable

--- book/RxJava리액티브프로그래밍/src/main/java/chap01/MainFlowable.java
+++ book/RxJava리액티브프로그래밍/src/main/java/chap01/MainFlowable.java
@@ -1,6 +1,7 @@
 package chap01;
 
 import io.reactivex.*;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import org.reactivestreams.Subscriber;
@@ -13,7 +14,8 @@ public class MainFlowable {
 //    executeExampleBasic();
 //    executeSubscribeCancel();
 //    requestMethodSample();
-    disposableExample();
+//    disposableExample();
+    compositeDisposable();
   }
 
   private static void executeExampleBasic() throws InterruptedException {
@@ -153,6 +155,33 @@ public class MainFlowable {
     Thread.sleep(500L);
     disposable.dispose();
 
+  }
+
+  public static void compositeDisposable() throws InterruptedException {
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    compositeDisposable.add(
+        Flowable.range(1, 10)
+            .doOnCancel(() -> System.out.println("No.1 canceled"))
+            .observeOn(Schedulers.computation())
+            .subscribe(data -> {
+              Thread.sleep(100L);
+              System.out.println("No1. data = " + data);
+            })
+    );
+
+    compositeDisposable.add(
+        Flowable.range(1, 10)
+            .doOnCancel(() -> System.out.println("No.2 canceled"))
+            .observeOn(Schedulers.computation())
+            .subscribe(data -> {
+              Thread.sleep(100L);
+              System.out.println("No2. data = " + data);
+            })
+    );
+
+    Thread.sleep(500L);
+    compositeDisposable.dispose();
   }
 
 }
