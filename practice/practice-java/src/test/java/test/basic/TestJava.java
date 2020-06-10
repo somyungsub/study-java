a6059a264bbfc7b49fa48e47ed0e3d6ef8c06073
myungsubso <gkdldy5@naver.com>
2020-06-10 20:59:04 +0900

    Add : set test

--- practice/practice-java/src/test/java/test/basic/TestJava.java
+++ practice/practice-java/src/test/java/test/basic/TestJava.java
@@ -3,10 +3,12 @@ package test.basic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class TestJava {
 
@@ -37,6 +39,61 @@ class TestJava {
     treeSet.add("d");
 
     System.out.println("treeSet = " + treeSet);
+
+  }
+
+  @Test
+  @DisplayName("AtomicInteger")
+  public void atomic() {
+    AtomicInteger atomicInteger = new AtomicInteger(10);
+    System.out.println("atomicInteger.get() = " + atomicInteger.get());
+  }
+
+  private static Hashtable<String, Integer> ht = new Hashtable<>();
+  private static HashMap<String, Integer> hm = new HashMap<>();
+  private static HashMap<String, Integer> hmSyn = new HashMap<>();
+  private static Map<String, Integer> hmSyn2 = Collections.synchronizedMap(new HashMap<>());
+  private static ConcurrentHashMap<String, Integer> chm = new ConcurrentHashMap<>();
+
+  @Test
+  @DisplayName("map-멀티쓰레드")
+  public void map() {
+
+    ExecutorService es = Executors.newFixedThreadPool(10);
+
+    for( int j = 0 ; j < 10; j++ ){
+      es.execute(new Runnable() {
+        @Override
+        public void run() {
+          for( int i = 0; i < 1000; i++ ){
+
+            String key = String.valueOf(i);
+
+            ht.put(key, i);
+            hm.put(key, i);
+            chm.put(key, i);
+            hmSyn2.put(key, i);
+
+            synchronized (hmSyn) {
+              hmSyn.put(key, i);
+            }
+          }
+        }
+      });
+    }
+
+    es.shutdown();
+    try {
+      es.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("Hashtable size is "+ ht.size());
+    System.out.println("HashMap size is "+ hm.size());
+    System.out.println("ConcurrentHashMap size is "+ chm.size());
+    System.out.println("HashMap(synchronized) size is "+ hmSyn.size());
+    System.out.println("synchronizedMap size is "+ hmSyn2.size());
 
   }
 }
