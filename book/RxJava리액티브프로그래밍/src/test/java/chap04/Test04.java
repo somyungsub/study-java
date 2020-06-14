7cb4e7804032cbfcab87153931ca889488f1856e
myungsubso <gkdldy5@naver.com>
2020-06-14 18:13:14 +0900

    Add : RxJava, 4장 예제 - 생성 나머지

--- book/RxJava리액티브프로그래밍/src/test/java/chap04/Test04.java
+++ book/RxJava리액티브프로그래밍/src/test/java/chap04/Test04.java
@@ -69,5 +69,66 @@ public class Test04 {
     Thread.sleep(5000L);
   }
 
+  @Test
+  @DisplayName("timer")
+  public void ex4_8() throws Exception {
+    final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm:ss.SSS");
+
+    System.out.println("시작시각 : " + LocalDateTime.now().format(formatter));
+
+    Flowable<Long> flowable = Flowable.timer(1000L, TimeUnit.MILLISECONDS);
+
+    flowable.subscribe(data -> {
+          String name = Thread.currentThread().getName();
+          String time = LocalDateTime.now().format(formatter);
+          System.out.println(name + ": " + time + ": date=" + data);
+        },
+        throwable -> System.out.println("throwable = " + throwable),
+        () -> System.out.println("완료!!")
+    );
+
+    Thread.sleep(1500L);
+  }
+
+  @Test
+  @DisplayName("defer")
+  public void ex4_10() throws Exception {
+    Flowable<LocalDateTime> flowable = Flowable.defer(() -> Flowable.just(LocalDateTime.now()));
+
+    flowable.subscribe(new DebugSubscriber<>("No. 1"));
+    Thread.sleep(2000L);
+    flowable.subscribe(new DebugSubscriber<>("No. 2"));
+  }
+
+  @Test
+  @DisplayName("empty")
+  public void ex4_11() {
+    Flowable
+        .empty()
+        .subscribe(new DebugSubscriber<>());
+
+    System.out.println("=========================");
+
+    Flowable
+        .just("A","B","C","D","","E","F")
+        .flatMap(string -> {
+          if (string.isBlank()) {
+            System.out.println("빈값");
+            return Flowable.empty();
+          }
+          return Flowable.just(string.concat("^^"));
+        })
+        .subscribe(new DebugSubscriber<>());
+
+  }
+
+  @Test
+  @DisplayName("never")
+  public void ex4_13() {
+    Flowable
+        .never()
+        .subscribe(new DebugSubscriber<>());
+  }
+
   
 }
