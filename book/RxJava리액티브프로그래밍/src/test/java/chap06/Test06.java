1adee1c95a4b61933d039aa8471c2885fb8861fe
myungsubso <gkdldy5@naver.com>
2020-06-22 06:59:29 +0900

    Add : RxJava, 6장 디버깅 및 테스트 - doOnComplete

--- book/RxJava리액티브프로그래밍/src/test/java/chap06/Test06.java
+++ book/RxJava리액티브프로그래밍/src/test/java/chap06/Test06.java
@@ -2,6 +2,7 @@ package chap06;
 
 import common.DebugSubscriber;
 import io.reactivex.Flowable;
+import io.reactivex.functions.Action;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,15 @@ public class Test06 {
         .doOnNext(data -> System.out.println("--- 기존 데이터 : " + data))
         .filter(data -> data % 2 == 0)
         .doOnNext(data -> System.out.println("--- filter 적용 후 데이터 : " + data))
+        .subscribe(new DebugSubscriber<>());
+  }
+
+  @Test
+  @DisplayName("doOnComplete")
+  public void ex6_4() throws Exception{
+
+    Flowable.range(1, 5)
+        .doOnComplete(() -> System.out.println("doOnComplete"))
         .subscribe(new DebugSubscriber<>());
   }
 }
