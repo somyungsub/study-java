d28f820c3afaee106bd7261d68300c140c4040d4
myungsubso <gkdldy5@naver.com>
2020-06-28 16:29:29 +0900

    Add : 다이내믹 프로그래밍, 5장 실전문제 - 연습문제 5-3 대각선 추가 경로 재귀,DP

--- book/다이내믹프로그래밍/src/main/java/chap05_실전문제/Practice05.java
+++ book/다이내믹프로그래밍/src/main/java/chap05_실전문제/Practice05.java
@@ -5,7 +5,10 @@ package chap05_실전문제;
  */
 public class Practice05 {
 
-  public int 좌표경로수(int x, int y) {
+  /*
+    연습문제 5-1 좌표 경로수
+   */
+  public int 좌표경로수5_1(int x, int y) {
     int[][] arr = new int[y + 1][x + 1];
 
     // x -> 1열
@@ -27,4 +30,70 @@ public class Practice05 {
 
     return arr[y][x];
   }
+
+  /*
+     연습문제 5-2 좌표경로 차단 추가 -> 인풋 애매해서 보류
+   */
+  public int 좌표경로차단5_2(int x, int y, int[][] breakPath) {
+
+    // 차단 경로 breakPath
+    int[][] arr = new int[y + 1][x + 1];
+
+    // x -> 1열
+    for (int i = 1; i <= x; i++) {
+      arr[0][i] = 1;
+    }
+
+    // y -> 1행
+    for (int i = 1; i <= y; i++) {
+      arr[i][0] = 1;
+    }
+
+    // 나머지
+    for (int i = 1; i <= y; i++) {
+      for (int j = 1; j <= x; j++) {
+        arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+      }
+    }
+
+    return arr[y][x];
+  }
+
+  /*
+    연습문제 5-3 대각선 추가 경로
+   */
+
+  public int 대각선추가5_3_예제2_재귀(int m, int n) {
+    if (m == 0 && n == 0) {
+      return 1;
+    }
+
+    if (m == 0 || n == 0) {
+      return 1;
+    }
+
+    return 대각선추가5_3_예제2_재귀(m - 1, n) + 대각선추가5_3_예제2_재귀(m, n - 1) + 대각선추가5_3_예제2_재귀(m - 1, n - 1);
+  }
+
+  public int 대각선추가5_3_예제2_DP(int m, int n) {
+    int[][] arr = new int[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+      arr[i][0] = 1;
+    }
+
+    for (int j = 0; j <= n; j++) {
+      arr[0][j] = 1;
+    }
+
+    // 나머지
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i][j - 1];
+      }
+    }
+
+    return arr[m][n];
+  }
+
 }
--- book/다이내믹프로그래밍/src/test/java/chap05_실전문제/Practice05Test.java
+++ book/다이내믹프로그래밍/src/test/java/chap05_실전문제/Practice05Test.java
@@ -12,13 +12,43 @@ class Practice05Test {
   @Test
   @DisplayName("연습문제 5-1")
   public void practice5_1() {
-    assertEquals(0, practice05.좌표경로수(0, 0));
-    assertEquals(1, practice05.좌표경로수(0, 10));
-    assertEquals(1, practice05.좌표경로수(10, 0));
+    assertEquals(0, practice05.좌표경로수5_1(0, 0));
+    assertEquals(1, practice05.좌표경로수5_1(0, 10));
+    assertEquals(1, practice05.좌표경로수5_1(10, 0));
 
-    assertEquals(2, practice05.좌표경로수(1, 1));
-    assertEquals(10, practice05.좌표경로수(2, 3));
-    assertEquals(10, practice05.좌표경로수(3, 2));
+    assertEquals(2, practice05.좌표경로수5_1(1, 1));
+    assertEquals(10, practice05.좌표경로수5_1(2, 3));
+    assertEquals(10, practice05.좌표경로수5_1(3, 2));
+  }
+
+  @Test
+  @DisplayName("연습문제5-2")
+  public void practice5_2() {
+    // 인풋??
+  }
+
+  @Test
+  @DisplayName("연습문제 5-3 - 재귀")
+  public void practice5_3_재귀() {
+    assertEquals(1, practice05.대각선추가5_3_예제2_재귀(0, 0));
+    assertEquals(1, practice05.대각선추가5_3_예제2_재귀(1, 0));
+    assertEquals(1, practice05.대각선추가5_3_예제2_재귀(0, 1));
+
+    assertEquals(3, practice05.대각선추가5_3_예제2_재귀(1, 1));
+    assertEquals(13, practice05.대각선추가5_3_예제2_재귀(2, 2));
+    assertEquals(25, practice05.대각선추가5_3_예제2_재귀(2, 3));
+  }
+
+  @Test
+  @DisplayName("연습문제 5-3 - DP")
+  public void practice5_3_DP() {
+    assertEquals(1, practice05.대각선추가5_3_예제2_DP(0, 0));
+    assertEquals(1, practice05.대각선추가5_3_예제2_DP(1, 0));
+    assertEquals(1, practice05.대각선추가5_3_예제2_DP(0, 1));
+
+    assertEquals(3, practice05.대각선추가5_3_예제2_DP(1, 1));
+    assertEquals(13, practice05.대각선추가5_3_예제2_DP(2, 2));
+    assertEquals(25, practice05.대각선추가5_3_예제2_DP(2, 3));
   }
 
 }
