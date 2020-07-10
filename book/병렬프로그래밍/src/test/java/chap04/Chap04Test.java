bacd92d936eb631958f6550a917055087ffada2c
myungsubso <gkdldy5@naver.com>
2020-07-10 16:34:15 +0900

    Add : 병렬프로그래밍, 5장 구성단위 - 블로킹큐 사용

--- book/병렬프로그래밍/src/test/java/chap04/Chap04Test.java
+++ book/병렬프로그래밍/src/test/java/chap04/Chap04Test.java
@@ -3,6 +3,8 @@ package chap04;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
 
 class Chap04Test {
@@ -29,5 +31,33 @@ class Chap04Test {
     System.out.println("privateLock.getCount() = " + privateLock.getCount());
   }
 
+  @Test
+  @DisplayName("블로킹큐")
+  public void blocking_queue() throws InterruptedException {
+    BlockingQueue bq = new ArrayBlockingQueue(10);
+    System.out.println("bq.remainingCapacity() = " + bq.remainingCapacity());
+    System.out.println("bq.size() = " + bq.size());
+
+    for (int i = 0; i <11 ; i++) {
+      bq.put(i);
+      if (bq.size() == 10) {
+        System.out.println("bq.size() = " + bq.size());
+        System.out.println("bq.take()1 = " + bq.take());
+        System.out.println("bq.take()2 = " + bq.take());
+        System.out.println("bq.take()3 = " + bq.take());
+        System.out.println("bq.take()4 = " + bq.take());
+//        bq.forEach(o -> {
+//          try {
+//            System.out.println("o = " + bq.take());
+//          } catch (InterruptedException e) {
+//            e.printStackTrace();
+//          }
+//        });
+      }
+    }
+    System.out.println("===============");
+    System.out.println("bq.size() = " + bq.size());
+
+  }
 
 }
