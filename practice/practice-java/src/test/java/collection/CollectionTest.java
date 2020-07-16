509845770f70a4af60a260cd752d313138fe7e97
myungsubso <gkdldy5@naver.com>
2020-07-16 18:14:35 +0900

    Add : 컬렉션 관련 불변객체 만들기

--- practice/practice-java/src/test/java/collection/CollectionTest.java
+++ practice/practice-java/src/test/java/collection/CollectionTest.java
@@ -19,25 +19,36 @@ public class CollectionTest {
 
     DataTest dataTest = map.get(1);
 
-//    dataTest.setNum(4);
-//    dataTest.setName("444");
+    dataTest.setNum(4);
+    dataTest.setName("444");
 
     System.out.println("map = " + map);
   }
 
   @Test
-  @DisplayName("불변객체")
-  public void 불변(){
+  @DisplayName("가변")
+  public void 가변(){
     Map<Integer, DataTest> map = getMap();
     HashMap<Integer, DataTest> immutable = new HashMap<>(map);
 
     DataTest dataTest = immutable.get(1);
 
-//    dataTest.setNum(4);
-//    dataTest.setName("444");
+    dataTest.setNum(4);
+    dataTest.setName("444");
 
     System.out.println("map = " + map);
     System.out.println("immutable = " + immutable);
+  }
+
+  @Test
+  @DisplayName("불변")
+  public void 불변(){
+    Map<Integer, DataImmutableTest> immutableMap = getImmutableMap();
+    DataImmutableTest dataImmutableTest = immutableMap.get(1);
+
+    System.out.println("immutableMap = " + immutableMap);
+    System.out.println("dataImmutableTest = " + dataImmutableTest);
+
   }
 
   @Test
@@ -65,13 +76,39 @@ public class CollectionTest {
     return map;
   }
 
+  private Map<Integer, DataImmutableTest> getImmutableMap() {
+    Map<Integer, DataImmutableTest> map = new HashMap<>();
+
+    map.put(1, new DataImmutableTest(1,"11"));
+    map.put(2, new DataImmutableTest(2,"22"));
+    map.put(3, new DataImmutableTest(3,"33"));
+
+    return map;
+  }
+
   @Data
   @AllArgsConstructor
   class DataTest {
 
-    // 불변객체로 만들어야 가능
     private int num;
     private String name;
+
+    @Override
+    public String toString() {
+      return "DataTest{" +
+          "num=" + num +
+          ", name='" + name + '\'' +
+          '}';
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  class DataImmutableTest {
+
+    // 불변객체로 만들어야 가능
+    private final int num;
+    private final String name;
 
     @Override
     public String toString() {
