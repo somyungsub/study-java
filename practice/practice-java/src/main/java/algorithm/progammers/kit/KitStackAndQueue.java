7f7e9fa6b5240dd8f842a59ad5a817fd31e5b64e
myungsubso <gkdldy5@naver.com>
2020-08-06 00:05:20 +0900

    Add : 프로그래머스 문제키트, 스택/큐 (기능개발)

--- practice/practice-java/src/main/java/algorithm/progammers/kit/KitStackAndQueue.java
+++ practice/practice-java/src/main/java/algorithm/progammers/kit/KitStackAndQueue.java
@@ -1,5 +1,11 @@
 package algorithm.progammers.kit;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Stream;
+
 public class KitStackAndQueue {
 
   public int[] solutionStockPrice(int[] prices) {
@@ -19,5 +25,65 @@ public class KitStackAndQueue {
       answer[i] = count;
     }
     return  answer;
+  }
+
+  public int[] solutionFeatureDevelopment(int[] progresses, int[] speeds) {
+
+    List<Integer> list = new ArrayList<>();
+    Queue<Integer> queue = new ArrayDeque<>();
+
+    while (true) {
+      for (int i = 0; i < progresses.length; i++) {
+        int progress = progresses[i];
+        if (progress < 100) {
+          progresses[i] = progress + speeds[i];
+          if (progresses[i] == 100 && !queue.contains(i)) {
+            queue.add(i);
+          }
+        } else {
+          if (!queue.contains(i)) {
+            queue.add(i);
+          }
+        }
+      }
+
+      if (isComplete(progresses)) {
+        break;
+      }
+    }
+
+    int count = 0;
+    int max = queue.peek();
+    while (!queue.isEmpty()) {
+
+      Integer present = queue.poll();
+      Integer next = queue.isEmpty() ? present : queue.peek();
+      count++;
+      if (present > max) {
+        max = present;
+      }
+
+      if (max + 1 == next || queue.isEmpty()) {
+        list.add(count);
+        count = 0;
+      }
+
+    }
+
+    int[] answer = new int[list.size()];
+    for (int j = 0; j < list.size(); j++) {
+      answer[j] = list.get(j);
+    }
+
+    return answer;
+  }
+
+  private boolean isComplete(int[] progresses) {
+    for (int progress : progresses) {
+      if (progress < 100) {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- practice/practice-java/src/test/java/algorithm/progammers/kit/KitStackAndQueueTest.java
+++ practice/practice-java/src/test/java/algorithm/progammers/kit/KitStackAndQueueTest.java
@@ -20,4 +20,28 @@ class KitStackAndQueueTest {
     }
   }
 
+  @Test
+  @DisplayName("기능개발")
+  public void test2(){
+    int[] progresses = {93, 30, 55};
+    int[] speeds = {1, 30, 5};
+
+    int[] ints = kit.solutionFeatureDevelopment(progresses, speeds);
+    for (int item : ints) {
+      System.out.println("item = " + item);
+    }
+  }
+
+  @Test
+  @DisplayName("기능개발")
+  public void test2_1(){
+    int[] progresses = {93, 30, 55, 50};
+    int[] speeds = {1, 30, 5, 10};
+
+    int[] ints = kit.solutionFeatureDevelopment(progresses, speeds);
+    for (int item : ints) {
+      System.out.println("item = " + item);
+    }
+  }
+
 }
