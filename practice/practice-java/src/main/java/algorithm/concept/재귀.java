91e723ff8cc5db3cee25d80cdf5e24a7e7b1baeb
myungsubso <gkdldy5@naver.com>
2020-08-12 14:48:23 +0900

    Add : 권오흠교수님 유튜브 강의, 순열

--- practice/practice-java/src/main/java/algorithm/concept/재귀.java
+++ practice/practice-java/src/main/java/algorithm/concept/재귀.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class 멱집합 {
+public class 재귀 {
 
+
+  // 4. 멱집합
   /*
     부분집합
     {a,b,c,d}
@@ -31,6 +33,7 @@ public class 멱집합 {
      - {3,4}의 모든 부분집합에 {1,2}를 추가한 집합들을 나열한다
    */
   List<String> list = new ArrayList<>();
+
   public void powerSet(int[] P, int[] S) {
     if (S.length == 0) {
       System.out.println(Arrays.toString(P));
@@ -39,12 +42,12 @@ public class 멱집합 {
       int[] ints = Arrays.copyOf(P, P.length + 1);
       ints[ints.length - 1] = t;
 
-      powerSet(P, Arrays.copyOfRange(S,1, S.length));             // t를 포함하지 않은 집합 모두 출력
-      powerSet(ints, Arrays.copyOfRange(S,1, S.length));     // t를 반드시 포함하는 부분 집합들
+      powerSet(P, Arrays.copyOfRange(S, 1, S.length));             // t를 포함하지 않은 집합 모두 출력
+      powerSet(ints, Arrays.copyOfRange(S, 1, S.length));     // t를 반드시 포함하는 부분 집합들
     }
   }
 
-//  private char data[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+  //  private char data[] = {'a', 'b', 'c', 'd', 'e', 'f'};
   private char data[] = {'a', 'b', 'c', 'd'};
   private int n = data.length;
   private boolean[] include = new boolean[n];
@@ -68,6 +71,45 @@ public class 멱집합 {
     // 오른쪽 (포함하는)
     include[k] = true;
     powerSet2(k + 1);
+  }
+
+
+  // 5. 순열
+  public void printPerm(int[] prefix, int[] S) {
+    // 1인 경우? 원소 1개 -> 1개로 만들 수 있는 순열은 1개이므로  S.length == 1로 해도 무방
+    if (S.length == 0) {
+      System.out.println(Arrays.toString(prefix));
+      return;
+    }
+    for (int i = 0; i < S.length; i++) {
+      int[] prefixArr = Arrays.copyOf(prefix, prefix.length + 1);
+      prefixArr[prefixArr.length - 1] = S[i];
+      int[] ints = Arrays.copyOfRange(S, 1, S.length);
+      printPerm(prefixArr, ints);
+    }
+  }
+
+
+  int[] permData = {1, 2, 3, 4};
+  int n5 = 4;
+
+  public void perm(int k) {
+    if (k == n5) {
+      System.out.println(Arrays.toString(permData));
+      return;
+    }
+
+    for (int i = k; i < n5; i++) {
+      swap(permData, k, i); // data[k]와 data[i] 스왑
+      perm(k + 1);
+      swap(permData, k, i); // 리커전에 들어가기 전과 후에 데이터의 동일성이 유지되도록
+    }
+  }
+
+  private void swap(int[] data, int k, int i) {
+    int temp = data[k];
+    data[k] = data[i];
+    data[i] = temp;
   }
 
 }
--- practice/practice-java/src/test/java/algorithm/concept/ConceptTest.java
+++ practice/practice-java/src/test/java/algorithm/concept/ConceptTest.java
@@ -3,22 +3,33 @@ package algorithm.concept;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ConceptTest {
-  멱집합 powerSet = new 멱집합();
+  재귀 recur = new 재귀();
 
   @Test
   @DisplayName("멱집합")
   public void test() {
     int[] S = {1, 2, 3, 4};
-    powerSet.powerSet(new int[0], S);
+    recur.powerSet(new int[0], S);
   }
 
   @Test
   @DisplayName("멱집합2")
   public void test2(){
-    powerSet.powerSet2(0);
+    recur.powerSet2(0);
+  }
+
+  @Test
+  @DisplayName("순열")
+  public void test3() {
+    int[] arr = {1, 2, 3, 4};
+    recur.printPerm(new int[0], arr);
+  }
+
+  @Test
+  @DisplayName("순열2")
+  public void test4() {
+    recur.perm(0);
   }
 
 }
