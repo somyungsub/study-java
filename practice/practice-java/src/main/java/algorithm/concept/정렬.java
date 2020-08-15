27f2ec2b8f9c88d5c8d710c8306b72ae6a45aeec
myungsubso <gkdldy5@naver.com>
2020-08-15 13:12:17 +0900

    Add : 권오흠 교수님 유튜브 강의, 정렬 - 합병정렬(머지소트)

--- practice/practice-java/src/main/java/algorithm/concept/정렬.java
+++ practice/practice-java/src/main/java/algorithm/concept/정렬.java
@@ -2,6 +2,63 @@ package algorithm.concept;
 
 public class 정렬 {
 
+  public void selectionSort(int[] arr) {
+
+  }
+
+  public void bubbleSort(int[] arr) {
+
+  }
+
+  public void insertSort(int[] arr) {
+
+  }
+
+  public void mergeSort(int[] arr, int p, int r) {
+    // 1. 나누어 정렬
+    // 2. 재귀
+    // 3. 추가배열 이용. 정렬된 2개의 배열을 합쳐서 전체 정렬
+
+    if (p < r) {
+      int q = (p + r) / 2;          // p,r ,의 중간 지점
+      mergeSort(arr, p, q);         // 전반부 정렬
+      mergeSort(arr, q + 1, r); // 후반부 정렬
+      merge(arr, p, q, r);          // 합병
+    }
+  }
+
+  private void merge(int[] arr, int p, int q, int r) {
+    // 정렬되어 있는 두배열 A[p...q], A[q+1...r]을 합하여
+    // 정렬된 하나의 배열 A[p...r]을 만든다.
+    int i = p, j = q + 1, k = p;
+    int[] tmp = new int[arr.length];
+
+    // i부터 (왼쪽), j부터(오른쪽) 비교 -> tmp에 저장
+    while (i <= q && j <= r) {
+      if (arr[i] <= arr[j]) {
+        tmp[k++] = arr[i++];
+      } else {
+        tmp[k++] = arr[j++];
+      }
+    }
+
+    // 한쪽 끝나고 남은 부분 정렬
+    // 앞부분이 남은 경우
+    while (i <= q) {
+      tmp[k++] = arr[i++];
+    }
+    // 뒷부분이 남은 경우
+    while (j <= r) {
+      tmp[k++] = arr[j++];
+    }
+
+    for (int l = p; l <= r; l++) {
+      arr[l] = tmp[l];
+    }
+
+  }
+
+
   // 힙정렬
   public void maxHeapify_recur(int[] heapArr, int rootIdx) {
 
@@ -36,4 +93,5 @@ public class 정렬 {
     // 2. 자식노드 중 큰놈 인덱스랑 자신 비교 -> 스왑
 
   }
+
 }
---
+++ practice/practice-java/src/test/java/algorithm/concept/정렬ConceptTest.java
@@ -0,0 +1,22 @@
+package algorithm.concept;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class 정렬ConceptTest {
+  정렬 sort = new 정렬();
+
+  @Test
+  @DisplayName("합병정렬")
+  public void test2(){
+    int[] arr = {5, 1, 3, 4, 123, 5452, 112};
+    sort.mergeSort(arr, 0, arr.length - 1); // 0~마지막 인덱스
+
+    System.out.println(Arrays.toString(arr));     // 1,3,4,5,112,123,5452
+  }
+
+}
