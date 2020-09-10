92cd64c9af47608e32b51ad85e5333db33f68fc0
myungsubso <gkdldy5@naver.com>
2020-09-10 09:22:49 +0900

    Add : 2장 - 입출금 내역 분석기, SRP

---
+++ book/실전자바소프트웨어개발/src/main/java/chapter_02/BankStatementCSVParser.java
@@ -0,0 +1,28 @@
+package chapter_02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class BankStatementCSVParser {
+
+  private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+  private BankTransaction parseFromCSV(final String line) {
+    final String[] columns = line.split(",");
+
+    final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+    final double amount = Double.parseDouble(columns[1]);
+    final String description = columns[2];
+
+    return new BankTransaction(date, amount, description);
+  }
+
+  public List<BankTransaction> parseLinesFromCSV(final List<String> lines) {
+    return lines.stream()
+      .map(this::parseFromCSV)
+      .collect(toList());
+  }
+}
---
+++ book/실전자바소프트웨어개발/src/main/java/chapter_02/BankTransactionAnalyzerSRP.java
@@ -0,0 +1,41 @@
+package chapter_02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankTransactionAnalyzerSRP {
+  private static final String RESOURCES = "book/실전자바소프트웨어개발/src/main/resources/";
+
+  public static void main(String[] args) throws IOException {
+    Path path = Paths.get(RESOURCES, "bank-data-simple.csv");
+    List<String> lines = Files.readAllLines(path);
+    final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+    List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+    double totalAmount = calculateTotalAmount(bankTransactions);
+    System.out.println("totalAmount = " + totalAmount);
+
+    double januaryAmount = selectInMonth(bankTransactions, Month.JANUARY);
+    System.out.println("januaryAmount = " + januaryAmount);
+
+  }
+
+  private static double calculateTotalAmount(List<BankTransaction> bankTransactions) {
+    return bankTransactions.stream()
+      .mapToDouble(BankTransaction::getAmount)
+      .sum();
+  }
+
+  private static double selectInMonth(List<BankTransaction> bankTransactions, Month month) {
+    return bankTransactions.stream()
+      .filter(bankTransaction -> bankTransaction.getDate().getMonth() == month)
+      .mapToDouble(BankTransaction::getAmount)
+      .sum();
+  }
+}
