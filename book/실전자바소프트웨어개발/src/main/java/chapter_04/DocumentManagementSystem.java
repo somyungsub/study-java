b48569bc83ee73895dddf82ea6eacd37022067ab
myungsubso <gkdldy5@naver.com>
2020-09-13 09:20:01 +0900

    Add : 4장 - 문서 관리 시스템,

---
+++ book/실전자바소프트웨어개발/src/main/java/chapter_04/DocumentManagementSystem.java
@@ -0,0 +1,15 @@
+package chapter_04;
+
+import java.util.List;
+
+public class DocumentManagementSystem {
+
+  public void importFile(String path) {
+
+  }
+
+  public List<Document> contents() {
+    return null;
+  }
+
+}
---
+++ book/실전자바소프트웨어개발/src/main/java/chapter_04/Importer.java
@@ -0,0 +1,8 @@
+package chapter_04;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface Importer {
+  Document importFile(File file) throws IOException;
+}
