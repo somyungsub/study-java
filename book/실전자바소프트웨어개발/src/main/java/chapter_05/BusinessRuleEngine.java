a2e946a8b4cc7ab0f458c0c2b6a5e89b6ef1e946
myungsubso <gkdldy5@naver.com>
2020-09-15 19:48:38 +0900

    Add : 5장 - 비즈니스 규칙 엔진

---
+++ book/실전자바소프트웨어개발/src/main/java/chapter_05/BusinessRuleEngine.java
@@ -0,0 +1,18 @@
+package chapter_05;
+
+import javax.swing.*;
+
+public class BusinessRuleEngine {
+
+  public void addAction(final Action action) {
+    throw new UnsupportedOperationException();
+  }
+
+  public int count() {
+    throw new UnsupportedOperationException();
+  }
+
+  public void run() {
+    throw new UnsupportedOperationException();
+  }
+}
