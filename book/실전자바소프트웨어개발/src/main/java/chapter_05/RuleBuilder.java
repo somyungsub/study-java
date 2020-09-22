80e3694b1b527e5b434149806b8ca0012d4bf28e
somyungsub <gkdldy5@gmail.com>
2020-09-22 20:05:33 +0900

    Add : RuleBuilder

---
+++ book/실전자바소프트웨어개발/src/main/java/chapter_05/RuleBuilder.java
@@ -0,0 +1,22 @@
+package chapter_05;
+
+public class RuleBuilder {
+
+  private Condition condition;
+  private Action action;
+
+  public RuleBuilder when(final Condition condition) {
+    this.condition = condition;
+    return this;
+  }
+
+  public RuleBuilder then(final Action action) {
+    this.action = action;
+    return this;
+  }
+
+  public Rule createRule() {
+    return new DefaultRule(condition, action);
+  }
+
+}
